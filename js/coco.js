94cb9af5bd2fac7865721df68ae1503458f045b7
jesse <jesseaborden@gmail.com>
2016-12-03 00:28:27 -0800

    Old project that I am restarting. Main desktop styles and sturcture done. Missing responsove styles

---
+++ js/coco.js
@@ -0,0 +1,101 @@
+$( "#waves" ).hide();
+
+
+$( "#row-three" ).hover(function() {
+  $( "#waves" ).fadeIn();
+
+});
+
+$("#row-three").mouseleave(function() {
+     $( "#waves" ).fadeOut();
+     
+  });
+
+$( ".sub-head-three" ).hide();
+
+$( "#row-three" ).hover(function() {
+  $( ".sub-head-three" ).fadeIn();
+
+});
+
+$("#row-three").mouseleave(function() {
+     $( ".sub-head-three" ).fadeOut();
+  });
+
+//Row Two Script
+
+$( "#limo" ).hide();
+
+
+$( "#row-two" ).hover(function() {
+  $( "#limo" ).fadeIn();
+
+});
+
+$("#row-two").mouseleave(function() {
+     $( "#limo" ).fadeOut();
+     
+  });
+
+$( ".sub-head-two" ).hide();
+
+$( "#row-two" ).hover(function() {
+  $( ".sub-head-two" ).fadeIn();
+
+});
+
+$("#row-two").mouseleave(function() {
+     $( ".sub-head-two" ).fadeOut();
+  });
+
+//Row One Script
+
+$( "#wolf" ).hide();
+
+
+$( "#row-one" ).hover(function() {
+  $( "#wolf" ).fadeIn();
+
+});
+
+$("#row-one").mouseleave(function() {
+     $( "#wolf" ).fadeOut();
+     
+  });
+
+$( ".sub-head-one" ).hide();
+
+$( "#row-one" ).hover(function() {
+  $( ".sub-head-one" ).fadeIn();
+
+});
+
+$("#row-one").mouseleave(function() {
+     $( ".sub-head-one" ).fadeOut();
+  });
+
+//Row Four Script
+
+$( "#flamingo" ).hide();
+
+
+$( "#row-four" ).hover(function() {
+  $( "#flamingo" ).fadeIn();
+
+});
+
+$("#row-four").mouseleave(function() {
+     $( "#flamingo" ).fadeOut();
+     
+  });
+
+$( ".sub-head-four" ).hide();
+
+$( "#row-four" ).hover(function() {
+  $( ".sub-head-four" ).fadeIn();
+
+});
+
+$("#row-four").mouseleave(function() {
+     $( ".sub-head-four" ).fadeOut();
+  });
